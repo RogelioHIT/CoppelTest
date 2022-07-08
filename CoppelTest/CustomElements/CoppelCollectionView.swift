6012a736b5c7b20f6625793b4dda26cdab19a39a
Rogelio <rogelio@dev.digono.com>
2022-07-08 14:21:51 -0500

    Custom collectionview configurado a dos columnas para la lista de peliculas

---
+++ CoppelTest/CustomElements/CoppelCollectionView.swift
@@ -0,0 +1,84 @@
+//
+//  CoppelCollectionView.swift
+//  CoppelTest
+//
+//  Created by Rogelio on 08/07/22.
+//
+
+import UIKit
+
+class CoppelCollectionView: UICollectionView {
+    
+    var movieCellId = "MovieCollectionViewCell"
+    var movies = [MovieViewModel]()
+
+    override init(frame: CGRect, collectionViewLayout layout: UICollectionViewLayout) {
+        super.init(frame: frame, collectionViewLayout: layout)
+        dataSource = self
+        delegate = self
+        
+        configCollectionView()
+        fillWithDummyData()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    func configCollectionView() {
+        
+        self.backgroundColor = UIColor.clear
+        self.backgroundView = UIView(frame: CGRect.zero)
+        let cellNib = UINib(nibName: "MovieCollectionViewCell", bundle: nil)
+        self.register(cellNib, forCellWithReuseIdentifier: movieCellId)
+    }
+    
+    func configUI() {
+        self.backgroundColor = .purple
+        self.backgroundView?.backgroundColor = .purple
+    }
+    
+    func fillWithDummyData() {
+        movies.append(MovieViewModel.getDummyModel())
+        movies.append(MovieViewModel.getDummyModel())
+        movies.append(MovieViewModel.getDummyModel())
+        movies.append(MovieViewModel.getDummyModel())
+        movies.append(MovieViewModel.getDummyModel())
+        movies.append(MovieViewModel.getDummyModel())
+        movies.append(MovieViewModel.getDummyModel())
+        movies.append(MovieViewModel.getDummyModel())
+        movies.append(MovieViewModel.getDummyModel())
+    }
+}
+
+extension CoppelCollectionView: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let viewModelForCell = movies[indexPath.item]
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: movieCellId, for: indexPath)
+        if let cell = cell as? MovieCollectionViewCell {
+            cell.config(with: viewModelForCell)
+        }
+        return cell
+    }
+}
+
+extension CoppelCollectionView: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+    
+        // aspect ratio of the designed cell
+        let aspectRatio: CGFloat = 62/121
+        let space: CGFloat = 24.0 // space left
+        let width: CGFloat = (collectionView.frame.size.width - space) / 2.0
+        return CGSize(width: width, height: width / aspectRatio)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 24.0
+    }
+    
+    
+}
