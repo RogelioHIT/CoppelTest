51c02be43e7bc3100a4cabb5fa4407ade46d6af0
Rogelio <rogelio@dev.digono.com>
2022-07-07 21:08:53 -0500

    WKWebView utilizado para la verificación del request token

---
+++ CoppelTest/Views/Login/WebViewController.swift
@@ -0,0 +1,54 @@
+//
+//  WebViewController.swift
+//  CoppelTest
+//
+//  Created by Rogelio on 07/07/22.
+//
+
+import UIKit
+import WebKit
+
+protocol WebViewAuthDelegate {
+    func authDismissWith(token: String, allowed: Bool)
+}
+
+class WebViewController: UIViewController {
+    
+    var webView: WKWebView!
+    var webViewAuthDelegate: WebViewAuthDelegate!
+    var token: String!
+    
+    override func loadView() {
+        super.loadView()
+        let webConfiguration = WKWebViewConfiguration()
+        webView = WKWebView(frame: .zero, configuration: webConfiguration)
+        webView.navigationDelegate = self
+        view = webView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupURL()
+    }
+    
+    func setupURL() {
+        let myURL = URL(string: "\(Endpoint.authenticate.urlString + token)")
+        let myRequest = URLRequest(url: myURL!)
+        webView.load(myRequest)
+    }
+}
+
+extension WebViewController: WKNavigationDelegate {
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        guard let url = webView.url else {
+            return
+        }
+
+        if url.lastPathComponent == "deny" || url.lastPathComponent == "allow" {
+            let allowed = url.lastPathComponent == "allow"
+            webViewAuthDelegate.authDismissWith(token: self.token, allowed: allowed)
+        }
+        
+    }
+}
