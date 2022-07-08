a4a5b963ccf8356ae002cb45dd4af290c484d035
Rogelio <rogelio@dev.digono.com>
2022-07-07 21:04:49 -0500

    extensiones utiles para view y string

---
+++ CoppelTest/Extensions/String+Extensions.swift
@@ -0,0 +1,18 @@
+//
+//  String+Extensions.swift
+//  CoppelTest
+//
+//  Created by Rogelio on 07/07/22.
+//
+
+import Foundation
+
+extension String {
+    
+    func isValidEmail() -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let emailPred = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailPred.evaluate(with: self)
+    }
+    
+}
---
+++ CoppelTest/Extensions/View+Extensions.swift
@@ -0,0 +1,21 @@
+//
+//  View+Extensions.swift
+//  CoppelTest
+//
+//  Created by Rogelio on 07/07/22.
+//
+
+import UIKit
+
+extension UIView {
+    func setGradientBackground(from color1:UIColor, to color2: UIColor) {
+        let colorTop =  color1.cgColor
+        let colorBottom = color2.cgColor
+                    
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [colorBottom, colorTop]
+        gradientLayer.locations = [0.0, 1.0]
+        gradientLayer.frame = self.bounds
+        self.layer.insertSublayer(gradientLayer, at:0)
+    }
+}
