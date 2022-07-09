03fcbbc6246d916827b4b8388f5b25d88812e3af
Rogelio <rogelio@dev.digono.com>
2022-07-09 18:24:05 -0500

    correccion en el nombre de la pelicula

--- CoppelTest/ViewModels/MovieViewModel.swift
+++ CoppelTest/ViewModels/MovieViewModel.swift
@@ -14,7 +14,7 @@ struct MovieViewModel {
     var movie: Movie!
     
     var movieName: String {
-        return movie.original_name.capitalized
+        return movie.original_title.capitalized
     }
     
     var moviePosterURL: URL? {
@@ -53,7 +53,7 @@ struct MovieViewModel {
     
     static func getDummyModel() -> MovieViewModel {
         let randomNumber = Int.random(in: 1..<100)
-        let dummyMovie = Movie(poster_path: "/7q448EVOnuE3gVAx24krzO7SNXM.jpg", backdrop_path: "/wcKFYIiVDvRURrzglV9kGu7fpfY.jpg", id: 31917, vote_average: 10.0, overview: "Based on the Pretty Little Liars series of young adult novels by Sara Shepard, the series follows the lives of four girls — Spencer, Hanna, Aria, and Emily — whose clique falls apart after the disappearance of their queen bee, Alison. One year later, they begin receiving messages from someone using the name \"A\" who threatens to expose their secrets — including long-hidden ones they thought only Alison knew.", release_date: "2016-08-03", original_language: "en", original_name: "Dummy #\(randomNumber)", video: false)
+        let dummyMovie = Movie(poster_path: "/7q448EVOnuE3gVAx24krzO7SNXM.jpg", backdrop_path: "/wcKFYIiVDvRURrzglV9kGu7fpfY.jpg", id: 31917, vote_average: 10.0, overview: "Based on the Pretty Little Liars series of young adult novels by Sara Shepard, the series follows the lives of four girls — Spencer, Hanna, Aria, and Emily — whose clique falls apart after the disappearance of their queen bee, Alison. One year later, they begin receiving messages from someone using the name \"A\" who threatens to expose their secrets — including long-hidden ones they thought only Alison knew.", release_date: "2016-08-03", original_language: "en", original_title: "Dummy #\(randomNumber)", video: false)
         return MovieViewModel(with: dummyMovie)
     }
 }
